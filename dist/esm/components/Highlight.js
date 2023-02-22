157f5b04dad39d79d79b31f1f5cd5f9df63850a4
cmf900 <cmf900@gmail.com>
2023-02-22 19:31:11 +0100

    tip

--- dist/esm/components/Highlight.js
+++ dist/esm/components/Highlight.js
@@ -4,12 +4,15 @@ export class Highlight extends Component {
     render() {
         const { position, onClick, onMouseOver, onMouseOut, comment, isScrolledTo, } = this.props;
         const { rects, boundingRect } = position;
+        const highlightClass = comment ? `Highlight__part Label__${comment.emoji}` : null;
         return (React.createElement("div", { className: `Highlight ${isScrolledTo ? "Highlight--scrolledTo" : ""}` },
-            comment ? (React.createElement("div", { className: "Highlight__emoji", style: {
+            comment ? (React.createElement("div", {
+                className: "Highlight__emoji", style: {
                     left: 20,
                     top: boundingRect.top,
-                } }, comment.emoji)) : null,
-            React.createElement("div", { className: "Highlight__parts" }, rects.map((rect, index) => (React.createElement("div", { onMouseOver: onMouseOver, onMouseOut: onMouseOut, onClick: onClick, key: index, style: rect, className: `Highlight__part Label__${comment.emoji}` }))))));
+                }
+            }, comment.emoji)) : null,
+            React.createElement("div", { className: "Highlight__parts" }, rects.map((rect, index) => (React.createElement("div", { onMouseOver: onMouseOver, onMouseOut: onMouseOut, onClick: onClick, key: index, style: rect, className: highlightClass }))))));
     }
 }
 export default Highlight;
